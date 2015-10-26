567026d18a1fdfae36c9fb52d665e01fdaf42025
johanna-codes <johacarvajalg@gmail.com>
2015-10-26 20:38:47 +1000

    Implementing BoW

--- kth/BoW/get_bin.m
+++ kth/BoW/get_bin.m
@@ -1,5 +1,18 @@
 function bin = get_bin(data_onevideo,mu)
 
-size (mu)
+[dim n_mu] = size (mu);
 
-bin =1;
+prev_dist = inf;
+
+for i=1:n_mu
+   
+    dist = norm(data_onevideo,mu(:,i);
+    
+    if dist < prev_dist
+        prev_dist = dist;
+        bin = i;
+    end
+    
+end
+
+bin
