032ae3b94d97b8811a91166512a55b46408ce3bd
johanna-codes <johacarvajalg@gmail.com>
2015-10-29 08:59:53 +1000

    BoW KTH

--- kth/BoW/get_hist.m
+++ kth/BoW/get_hist.m
@@ -6,24 +6,24 @@ function bin = get_hist(data_onevideo,mu)
 hist = zeros(n_mu,1);
 
 for  i=1:n_vectors
- 
+    
     one_vector = data_onevideo(:,i);
-
-prev_dist = inf;
-
-for m=1:n_mu
-   
-    dist = norm(one_vector - mu(:,m),2);
     
-    if dist < prev_dist
-        prev_dist = dist;
-        bin = m;
+    prev_dist = inf;
+    
+    for m=1:n_mu
+        
+        dist = norm(one_vector - mu(:,m),2);
+        
+        if dist < prev_dist
+            prev_dist = dist;
+            bin = m;
+        end
+        
+        
     end
+    hist(bin) = hist(bin) + 1;
     
-    
-end
-hist(bin) = hist(bin) + 1;
-
 end
 
 save('probando.mat', 'hist');
