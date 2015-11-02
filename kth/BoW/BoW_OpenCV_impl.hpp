97778b8b3a31feb6f1ac0d57c39e806f4d007420
johanna-codes <johacarvajalg@gmail.com>
2015-11-02 23:09:04 +1000

    BoW- getting hist for multi_features

--- kth/BoW/BoW_OpenCV_impl.hpp
+++ kth/BoW/BoW_OpenCV_impl.hpp
@@ -302,7 +302,7 @@ BoW::create_histograms_testing(int N_cent, const string path_run_folders, int se
         fr_idx.load( ssload_name_fr_idx.str() );  // Solo uso las pares: 2,4,6...
         fr_idx_2 = fr_idx/2; // Empieza en uno
         
-        cout << fr_idx_2 << endl;
+        cout << fr_idx_2.t() << endl;
 	
 	cout << fr_idx_2(0) << endl;
 	getchar();
