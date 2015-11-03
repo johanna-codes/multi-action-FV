1264d60ddfbbe2dc1a0e45e2eb4203cc45193fe7
johanna-codes <johacarvajalg@gmail.com>
2015-11-03 12:32:28 +1000

    Matlab: BoW- getting hist for multi_features

--- kth/BoW/BoW_train_test.m
+++ kth/BoW/BoW_train_test.m
@@ -32,50 +32,50 @@ dim = int2str(DIM);
 path_run_folders = '/home/johanna/codes/codes-git/multi-action-FV/trunk/kth';
 
 % %% TRAINING
-% for r=1:RUN
-% fprintf('RUN %d \n', r);
-% run = int2str(r);
-% 
-% %sc = int2str(4);
-% 
-% people = importdata(strcat(path_run_folders, '/run', run, '/rand_selection_run', run, '.dat'));
-% actionNames = importdata('actionNames.txt');
-% people_train = people(1:16);
-% 
-% 
-% n_pe_tr  = length(people_train);
-% n_actions = length(actionNames);
-% 
-% data_train = [];
-% labels_train = [];
-% 
-% display('Loading Training data');
-% for d = 1:4
-%     sc = int2str(d);
-%     for pe = 1: n_pe_tr
-% 
-%         for act = 1:n_actions
-% 
-%             load_name = strcat('./run', run,  '/Histograms_BoW_OpenCV/hist_', people_train(pe),'_',actionNames(act),'_d', sc, '_Ng', Ng, '.h5');
-%             sLoad = char(load_name);
-%             hinfo = hdf5info(sLoad);
-%             FV = hdf5read(hinfo.GroupHierarchy.Datasets(1));
-%             data_train = [data_train FV];
-%             labels_train = [labels_train (act - 1)]; % labels starts at 0
-% 
-%         end
-%     end
-% end
-% 
-% data_train = data_train';
-% labels_train = labels_train';
-% display('Training...');
-% model = svmtrain(labels_train, data_train, ['-s 0 -t 2 -b 1' ]); %CvSVM::RBF
-% save_name = strcat('./run', run,  '/svm_hist_model_Ng', Ng);
-% sSave= char(save_name);
-% display('Saving Model...');
-% save(sSave, 'model');
-% end
+for r=1:RUN
+fprintf('RUN %d \n', r);
+run = int2str(r);
+
+%sc = int2str(4);
+
+people = importdata(strcat(path_run_folders, '/run', run, '/rand_selection_run', run, '.dat'));
+actionNames = importdata('actionNames.txt');
+people_train = people(1:16);
+
+
+n_pe_tr  = length(people_train);
+n_actions = length(actionNames);
+
+data_train = [];
+labels_train = [];
+
+display('Loading Training data');
+for d = 1:4
+    sc = int2str(d);
+    for pe = 1: n_pe_tr
+
+        for act = 1:n_actions
+
+            load_name = strcat('./run', run,  '/Histograms_BoW_OpenCV/hist_', people_train(pe),'_',actionNames(act),'_d', sc, '_Ng', Ng, '.h5');
+            sLoad = char(load_name);
+            hinfo = hdf5info(sLoad);
+            FV = hdf5read(hinfo.GroupHierarchy.Datasets(1));
+            data_train = [data_train FV];
+            labels_train = [labels_train (act - 1)]; % labels starts at 0
+
+        end
+    end
+end
+
+data_train = data_train';
+labels_train = labels_train';
+display('Training...');
+model = svmtrain(labels_train, data_train, ['-s 0 -t 2 -b 1 -g 0.506 -c 312.5' ]); %CvSVM::RBF
+save_name = strcat('./run', run,  '/svm_hist_model_Ng', Ng);
+sSave= char(save_name);
+display('Saving Model...');
+save(sSave, 'model');
+end
 
 %% TESTING.
 %  Loading Testing data
