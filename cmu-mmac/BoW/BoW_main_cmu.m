468c98197e5973f013724c5d80f0e9c075782c8e
johanna-codes <johacarvajalg@gmail.com>
2015-11-09 20:01:56 +1000

    BoW CMU

--- cmu-mmac/BoW/BoW_main_cmu.m
+++ cmu-mmac/BoW/BoW_main_cmu.m
@@ -27,7 +27,7 @@ dim = int2str(14);
 path_run_folders = '/home/johanna/codes/codes-git/multi-action-FV/trunk/cmu-mmac';
 
 %% TRAINING
-BoW_train_cmu(RUN, Ng, path_run_folders);
+%BoW_train_cmu(RUN, Ng, path_run_folders);
 
 %% Testing
 all_run_acc = BoW_test_cmu(Ng,RUN,L,path_run_folders);
