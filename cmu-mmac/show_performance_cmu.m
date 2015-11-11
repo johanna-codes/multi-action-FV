5aa4bb7ff9fa85b12e12e455947d69e5aed3665e
johanna-codes <johacarvajalg@gmail.com>
2015-11-11 19:35:28 +1000

    CMU FV show performance

--- cmu-mmac/show_performance_cmu.m
+++ cmu-mmac/show_performance_cmu.m
@@ -1,7 +1,7 @@
 clear all
 clc
 
-%% 
+%%
 prompt = 'Segment Length? ';
 L = input(prompt);
 
@@ -9,12 +9,15 @@ prompt = 'last _Run? ';
 last_r = input(prompt);
 
 
-perf = [];
-for r=1:last_r
-load_name = strcat('./run',int2str(r),'/SysA_test_info_ONEsvm_Ng32_L',int2str(L),'.mat');  
-sLoad= char(load_name);
-load(sLoad) ;                                                                               
-perf = [perf test_info{4}]; 
+perf = zeros(1,12);
+for r=1:12
+    load_name = strcat('./run',int2str(r),'/SysA_test_info_ONEsvm_Ng32_L',int2str(L),'.mat');
+    sLoad= char(load_name);
+    if  (exist(sLoad))
+        load(sLoad);
+        perf(r) = test_info{4};
+    end
+    
 end
 
 
