0c898d81e51b12f13fe567c93181bb6700f9b0e2
johanna-codes <johacarvajalg@gmail.com>
2015-11-12 13:18:54 +1000

    Run only for the first 4 Videos

--- cmu-mmac/BoW/BoW_main_cmu.m
+++ cmu-mmac/BoW/BoW_main_cmu.m
@@ -18,7 +18,7 @@ L = input(prompt);
 
 % Ncent =64;
 % L = 15;
-RUN=12;
+RUN=4;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %RUN = 12;
@@ -27,7 +27,7 @@ dim = int2str(14);
 path_run_folders = '/home/johanna/codes/codes-git/multi-action-FV/trunk/cmu-mmac';
 
 %% TRAINING
-%BoW_train_cmu(RUN, Ng, path_run_folders);
+BoW_train_cmu(RUN, Ng, path_run_folders);
 
 %% Testing
 all_run_acc = BoW_test_cmu(Ng,RUN,L,path_run_folders);
